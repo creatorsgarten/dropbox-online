0aede63e1743cf1420f582a32163253b790ec6c2
Phoomparin Mano <poom@hey.com>
2022-04-24 11:18:15 +0700

    feat: init dropbox online

---
+++ src/routes/index.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	const name = 'Poom'
+</script>
+
+<main class="px-8 py-5 space-y-4">
+	<section>
+		<h1 class="text-2xl font-bold">Dropbox Online</h1>
+	</section>
+</main>
+
+<style></style>
