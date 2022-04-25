0dea8d587fc009858fd395b36d5f314ff6ce3234
Phoomparin Mano <poom@hey.com>
2022-04-25 19:15:41 +0700

    feat: ui

--- src/components/ContactList.svelte
+++ src/components/ContactList.svelte
@@ -6,10 +6,12 @@
 	<div class="space-y-3">
 		<h1 class="text-2xl">ส่งให้</h1>
 
-		<section class="flex flex-wrap items-center space-x-4">
+		<section
+			class="flex flex-col items-start space-y-4 sm:space-y-0 sm:space-x-4 sm:flex-row flex-wrap"
+		>
 			{#each $users as user}
 				<div
-					class="flex items-center bg-green-300 hover:bg-green-400 shadow-lg shadow-green-200 text-gray-800 px-4 py-3 rounded-lg w-full max-w-[240px] cursor-pointer"
+					class="flex items-center bg-green-300 hover:bg-green-400 shadow-lg shadow-green-100 text-gray-800 px-4 py-3 rounded-lg w-full max-w-[240px] cursor-pointer"
 				>
 					<div class="text-lg">
 						<span>{user.name}</span>
--- src/components/Logout.svelte
+++ src/components/Logout.svelte
@@ -6,7 +6,7 @@
 {#if $currentUser}
 	<button
 		on:click={logout}
-		class="bg-gray-500 hover:bg-gray-700 text-white px-3 py-2 rounded-lg shadow-lg shadow-gray-300 text-xs font-light"
+		class="bg-gray-500 hover:bg-gray-700 text-white px-3 py-2 rounded-lg shadow-lg shadow-gray-100 text-xs font-light"
 	>
 		ออกจากระบบ
 	</button>
