4e02461c3abf053c17f88b5d66b3d30e95401074
Phoomparin Mano <poom@hey.com>
2022-04-26 02:28:01 +0700

    chore: emoji

--- src/components/Outbox.svelte
+++ src/components/Outbox.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <main class="space-y-4">
-  <h1 class="text-2xl">ข้อความที่ฉันเขียนให้เพื่อน</h1>
+  <h1 class="text-2xl">📨 ข้อความที่ฉันเขียนให้เพื่อน</h1>
 
   <div class="grid items-center gap-4 gap-y-6 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6">
     {#each $outbox as note}
