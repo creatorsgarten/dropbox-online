5f5580b6691a8200ad77772ed1ba79a848850796
Phoomparin Mano <poom@hey.com>
2022-04-25 20:29:29 +0700

    feat: flag yourself

--- src/components/ContactList.svelte
+++ src/components/ContactList.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
-	import { users } from '../modules/user.store'
+	import c from 'classnames'
+
+	import { users, currentUser } from '../modules/user.store'
 </script>
 
 <div>
@@ -9,13 +11,20 @@
 		<section class="grid sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-3">
 			{#each $users as user}
 				<div
-					class="flex items-center bg-green-300 hover:bg-green-400 shadow-lg shadow-green-100 text-gray-800 px-4 py-3 rounded-lg w-full max-w-[240px] cursor-pointer"
+					class={c(
+						'flex items-center bg-green-300 hover:bg-green-400 shadow-lg shadow-green-100 text-gray-800 px-4 py-3 rounded-lg w-full max-w-[240px] cursor-pointer',
+						user.id === $currentUser.id && 'bg-gray-300 hover:bg-gray-400 shadow-gray-100'
+					)}
 				>
 					<div class="text-lg">
 						<span>{user.name ?? 'บุคคลนิรนาม'}</span>
 
 						{#if user.instagram}
 							<span class="text-sm">@{user.instagram}</span>
+						{/if}
+
+						{#if user.id === $currentUser.id}
+							<span class="text-sm">(ฉัน)</span>
 						{/if}
 					</div>
 				</div>
